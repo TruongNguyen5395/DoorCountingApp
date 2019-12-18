e34abcc0c69c2daca39e477e9b7f28e98883c0b4
TruongNguyen5395 <truongnguyen5395@gmail.com>
2019-12-18 14:27:47 +0700

    Dashboard 18.12

--- config/key1.js
+++ config/key1.js
@@ -1,8 +1,15 @@
 const mongoose = require ('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/targetDB',{ useNewUrlParser: true,useUnifiedTopology: true }, (err) => {
+// mongoose.connect('mongodb://localhost:27017/targetDB',{ useNewUrlParser: true }, (err) => {
+//     if (!err) { console.log('MongoDB connection Success.') }
+//     else { console.log('Error in DB Connection : ' + err) }
+// });
+
+
+mongoose.connect('mongodb+srv://truongnguyen5395:truong1995@cluster0-urh8c.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true }, (err) => {
     if (!err) { console.log('MongoDB connection Success.') }
     else { console.log('Error in DB Connection : ' + err) }
 });
 
 require ('../models/target.model');
+require ('../models/tempData');
--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
-//dbPassword = 'mongodb+srv://hoangtri:'+ encodeURIComponent('RibZvH1Bs5poe6nO') + '@cluster0-m3qdk.mongodb.net/test?retryWrites=true';
+// dbPassword = 'mongodb+srv://hoangtri:'+ encodeURIComponent('RibZvH1Bs5poe6nO') + '@cluster0-m3qdk.mongodb.net/test?retryWrites=true';
 
-dbPassword = 'mongodb://localhost:27017/auth1';
-
+//dbPassword = 'mongodb://localhost:27017/auth1';
+dbPassword = 'mongodb+srv://truongnguyen5395:truong1995@cluster0-urh8c.mongodb.net/test?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: dbPassword
